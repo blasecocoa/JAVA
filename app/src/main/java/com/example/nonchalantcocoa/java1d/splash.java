12ac2b9210c2949e1755fae2fc59ed313c19e89a
blasecocoa <abenayabv@gmail.com>
2018-12-11 01:06:28 +0800

    included splash

---
+++ app/src/main/java/com/example/nonchalantcocoa/java1d/splash.java
@@ -0,0 +1,23 @@
+package com.example.nonchalantcocoa.java1d;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        int secondsDelayed = 1;
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(splash.this, MainActivity.class));
+                finish();
+            }
+        }, secondsDelayed * 2000);
+    }
+}
